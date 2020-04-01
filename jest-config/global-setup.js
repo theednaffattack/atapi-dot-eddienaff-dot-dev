0ed4ebc1a2faa9424eb9bc2ace80861555c9a883
eddie naff <eddienaff@eddienaff.com>
2020-04-01 13:39:27 -0700

    Add jest global setiup for vscode

--- jest-config/global-setup.js
+++ jest-config/global-setup.js
@@ -1,12 +1,33 @@
+
+
 const dotenv = require("dotenv");
 
+
 // setup.js
-module.exports = async () => {
+module.exports = async function() {
   console.log("Jest setup")
   console.log("configuring environment variables")
   dotenv.config();
+
+  // conn = await testConn();
+  // done();
 };
+
 // teardown.js
 module.exports = async function() {
   console.log("Jest teardown")
+  // await conn.close();
+  // done();
 };
+
+
+
+// beforeAll(async done => {
+//   conn = await testConn();
+//   done();
+// });
+
+// afterAll(async done => {
+//   await conn.close();
+//   done();
+// });

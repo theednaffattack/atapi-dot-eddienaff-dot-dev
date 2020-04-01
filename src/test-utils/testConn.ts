e578f9cda6208a92d58ae3c5a9681ef4a95fde17
eddie naff <eddienaff@eddienaff.com>
2020-04-01 13:43:50 -0700

    Adjust test db settings

--- src/test-utils/testConn.ts
+++ src/test-utils/testConn.ts
@@ -1,16 +1,16 @@
-import { createConnection } from "typeorm";
+import { createConnection, Connection } from "typeorm";
 
-export const testConn = (drop: boolean = false) => {
+export const testConn = (drop = false): Promise<Connection> => {
   return createConnection({
     name: "default",
     type: "postgres",
     host: "localhost",
     port: 5432,
-    username: "eddienaff", // superuser
-    password: "eddienaff",
-    database: "sc_backend_testing",
+    username: process.env.POSTGRES_USER, // superuser
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_TEST_DB,
     dropSchema: drop,
     synchronize: true,
-    entities: [__dirname + "/../entity/*.*"]
+    entities: [__dirname + "/../entity/*.*"],
   });
 };
